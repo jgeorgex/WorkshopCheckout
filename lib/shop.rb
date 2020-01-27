e5db9ad9deddf60d8b675cb0e05af73a80d8c689
Julian George <jules104@gmail.com>
2020-01-27 16:46:52 +0000

    Add Shop class and checkout method

---
+++ lib/shop.rb
@@ -0,0 +1,9 @@
+class Shop
+
+    def checkout(sku)
+        if sku == "A"
+            return 50
+        end
+    end
+
+end
---
+++ spec/shop_spec.rb
@@ -0,0 +1,11 @@
+require "shop"
+
+describe "Shop" do
+
+    describe "#checkout" do
+        it "returns 50 when input is A" do
+            shop = Shop.new
+            expect(shop.checkout("A")).to eq(50)
+        end
+    end
+end
