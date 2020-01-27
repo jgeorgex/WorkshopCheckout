735944d2a6d7b06618db8b94e5db6424abc81477
Julian George <jules104@gmail.com>
2020-01-27 17:09:35 +0000

    Add get_values method

--- lib/shop.rb
+++ lib/shop.rb
@@ -3,7 +3,14 @@ class Shop
     def checkout(sku)
         if sku == "A"
             return 50
+        elsif sku == "AA"
+            return 100
         end
+    end 
+
+    def get_values(single_sku)
+        sku_values = { "A" => 50 }
+        single_sku = sku_values[single_sku]
     end
 
 end
--- spec/shop_spec.rb
+++ spec/shop_spec.rb
@@ -3,9 +3,26 @@ require "shop"
 describe "Shop" do
 
     describe "#checkout" do
+        
         it "returns 50 when input is A" do
             shop = Shop.new
             expect(shop.checkout("A")).to eq(50)
         end
+
+        it "returns 100 when input is AA" do
+            shop = Shop.new
+            expect(shop.checkout("AA")).to eq(100)
+        end
+
     end
+
+    describe "#get_values" do
+
+        it "returns 50 when input is A" do
+            shop = Shop.new
+            expect(shop.get_values("A")).to eq(50)
+        end
+
+    end
+
 end
